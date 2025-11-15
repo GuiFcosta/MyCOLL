74ca9cb75a943fb7308d6fa6281d3f6f72699cc2
GuiFcosta <gbigui0206@gmail.com>
2025-11-15 17:35:26 +0000

    feat: implement product and category management with API integration

--- StoreManagement/ViewModels/ProductViewModel.cs
+++ StoreManagement/ViewModels/ProductViewModel.cs
@@ -1,6 +1,28 @@
+using System.ComponentModel.DataAnnotations;
 namespace StoreManagement.ViewModels;
 
 public class ProductViewModel
 {
+    public int Id { get; set; }
     
+    [Required(ErrorMessage = "Name is required")]
+    public string? Name { get; set; }
+    
+    [Required(ErrorMessage = "Description is required")]
+    public string? Description { get; set; }
+    
+    [Range(0.01, double.MaxValue, ErrorMessage = "Base Price must be greater than zero")]
+    public decimal BasePrice { get; set; }
+    
+    [Range(0, 100, ErrorMessage = "Sell Tax must be between 0 and 100")]
+    public decimal SellTax { get; set; }
+    
+    [Required(ErrorMessage = "Condition is required")]
+    public string? Condition { get; set; }
+    
+    [Required(ErrorMessage =  "State is required")]
+    public string? State { get; set; }
+    
+    [Required(ErrorMessage = "Condition is required")]
+    public int CategoryId { get; set; }
 }
