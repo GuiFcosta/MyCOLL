3e4be2e0e3ff8757dbd0443f315d0ffc76b170ae
GuiFcosta <gbigui0206@gmail.com>
2025-11-16 08:25:52 +0000

    feat: initialize project structure with basic configuration and routing

--- MyCOLL/obj/Debug/net8.0/MyCOLL.AssemblyInfo.cs
+++ MyCOLL/obj/Debug/net8.0/MyCOLL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCOLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09e1ee2d892ff84fcdc60577e4461fd53f7e9489")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ca9cb75a943fb7308d6fa6281d3f6f72699cc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCOLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCOLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
