c92d2ba343f855a996bb7ac42f7385b80fc0af26
GuiFcosta <gbigui0206@gmail.com>
2025-11-20 12:25:46 +0000

    fix: update application URL in launchSettings and increment NuGet tool version

--- MyCOLL.Api/Models/DTO/CategoryDTO/CategoryReadDto.cs
+++
@@ -1,9 +0,0 @@
-namespace MyCOLL.Api.Models.DTO.CategoryDTO;
-
-public class CategoryReadDto
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-
-    public ICollection<string> Products { get; set; } = new List<string>();
-}
--- MyCOLL/obj/Debug/net8.0/MyCOLL.AssemblyInfo.cs
+++ MyCOLL/obj/Debug/net8.0/MyCOLL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCOLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d597560cc110dfa71fe7f37f9b4c6c3eb5cd797f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b19f783fc79f7b3dc2ecd3905a84671571eb4700")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCOLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCOLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
