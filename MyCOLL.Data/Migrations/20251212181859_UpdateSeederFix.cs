a8a2766dd79214e60a4ffb89f4782d5848eeddf5
GuiFcosta <gbigui0206@gmail.com>
2025-12-12 18:54:42 +0000

    migrations

---
+++ MyCOLL.Data/Migrations/20251212181859_UpdateSeederFix.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyCOLL.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSeederFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nif",
+                table: "AspNetUsers",
+                type: "nvarchar(9)",
+                maxLength: 9,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(9)",
+                oldMaxLength: 9);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nif",
+                table: "AspNetUsers",
+                type: "nvarchar(9)",
+                maxLength: 9,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(9)",
+                oldMaxLength: 9,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
