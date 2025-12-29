01a6653e71ee2da42b512ad9e316e9c6e44a815b
GuiFcosta <gbigui0206@gmail.com>
2025-12-29 19:16:06 +0000

    feat: update category and product management interfaces; enhance search and filter functionalities

--- MyCOLL/obj/Debug/net8.0/MyCOLL.AssemblyInfo.cs
+++ MyCOLL/obj/Debug/net8.0/MyCOLL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCOLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f828783e963bdf844e55a6f0492a06a2b435b464")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fd97ecbbbb2a8bca1151f6ea3e06c9acb870848")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCOLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCOLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
